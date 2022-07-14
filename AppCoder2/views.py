8827d859fc0fd8cedfd5439aec87642e316cd0d8
AdrianEspinola <adrianespinola87@gmail.com>
2022-07-14 14:13:06 -0300

    clase 19

--- AppCoder2/views.py
+++ AppCoder2/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import render
 from AppCoder2.models import Curso
 # Create your views here.
 
-def curso(sefl):
+def curso(self):
     curso = Curso(nombre="Curso de Python", comision=212)
     curso.save()
     texto = f"Curso creado: {curso.nombre} {curso.comision}"
     return HttpResponse(texto)
+
+def inicio(request):
+    return render(request,'AppCoder2/inicio.html')
+
+def cursos(request):
+    return render(request,'AppCoder2/cursos.html')
+
+def profesores(request):
+    return render(request,'AppCoder2/profesores.html')  
+
+def estudiantes(request):
+    return render(request,'AppCoder2/estudiantes.html')
+
+def entregables(request):
+    return render(request,'AppCoder2/entregables.html')
--- CLASE18/urls.py
+++ CLASE18/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from AppCoder2.views import curso
+from django.urls import path, include 
+#from AppCoder2.views import curso
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('curso/', curso),
+    path('AppCoder2/',include('AppCoder2.urls')),
+    #path('curso/', curso),
 ]
